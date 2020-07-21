047746cb7d33916c45ff2a9c699da4fe2111bcb5
Andrew Sievers <andrew.sievers@selu.edu>
2020-07-21 16:18:32 -0500

    bare minimum config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div>
+            <p>
+                Hello world!
+            </p>
+        </div>
+
+    );
+}
+
+export default App;
